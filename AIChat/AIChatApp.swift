a7ebb4845d10fc29d5f169d4eba2796dfa2f2698
lord-of-the-lost <ignatov.99@yandex.ru>
2025-08-10 16:58:44 +0300

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Николай Игнатов on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
